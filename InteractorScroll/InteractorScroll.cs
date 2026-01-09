999daefe6421cc0c4e54ffc0b953ea72b114503b
ShadowAPI <90363162+ShadowDragon128@users.noreply.github.com>
2026-01-09 00:19:52 -0600

    Change Scrolling Handling Behaviour

---
+++ InteractorScroll/InteractorScroll.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine;
+
+namespace InteractorScroll
+{
+    public class InteractorScroll : ResoniteMod
+    {
+        public override string Name => "InteractorScroll"; //"UIX.Displazor";
+        public override string Author => "ShadowAPI";
+        public override string Version => "3.1.0";
+        public override string Link => "https://github.com/ShadowDragon128/InteractorScroll";
+
+		[AutoRegisterConfigKey]
+		public static ModConfigurationKey<float> KEY_SPEED = new ModConfigurationKey<float>("scroll_speed", "How fast you scroll, default is 120.", () => 120);
+		public static ModConfiguration config;
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("usmx.ShadowAPI.InspectorScroll");
+            harmony.PatchAll();
+			config = GetConfiguration();
+        }
+
+        [HarmonyPatchCategory(nameof(InteractorScroll))]
+        internal class Patch
+        {
+            /// <summary>
+            /// Thanks to art0007i
+            /// https://github.com/art0007i/InspectorScroll
+            /// </summary>
+            /// <param name="__instance"></param>
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(InteractionHandler), "OnInputUpdate")]
+            public static void OnInputUpdate(InteractionHandler __instance)
+            {
+                if (__instance.Laser.CurrentTouchable as FrooxEngine.UIX.Canvas == null)
+                    return; // NAH!, keep looking.
+
+                IAxisActionReceiver axisActionReceiver = __instance.Laser.CurrentTouchable as IAxisActionReceiver;
+
+                if (axisActionReceiver != null && !__instance.InputInterface.ScreenActive)
+                {
+                    Elements.Core.float2 val = __instance.Inputs.Axis.Value.Value; // wa la no more reflections needed
+                    val *= new Elements.Core.float2(-1, 1);
+                    axisActionReceiver.ProcessAxis(__instance.Laser.TouchSource, val * config.GetValue(KEY_SPEED));
+                }
+                
+            }
+
+            /// <summary>
+            /// Thanks to zyntaks
+            /// https://github.com/furrz/NoTankControls
+            /// </summary>
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(InteractionHandler), "BeforeInputUpdate")]
+            public static void BeforeInputUpdate(InteractionHandler __instance)
+            {                
+                __instance.Inputs.Axis.RegisterBlocks = ((__instance.LocalUser.IsAppDashOpened && Userspace.Pointers.Count > 1) &&
+                    Userspace.Pointers[1].ActiveHandler.Laser.CurrentTouchable as FrooxEngine.UIX.Canvas != null) || 
+                    __instance.Laser.CurrentTouchable as FrooxEngine.UIX.Canvas != null; // Make it work with Userspace lasers :D
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# InspectorScroll
+# InteractorScroll
 
-A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that allows scrolling inspectors (and other ui panels) using your thumbstick / touchpad.<br>
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that allows scrolling Interactors (and other ui panels) using your thumbstick / touchpad.<br>
 
 Tested to work on Oculus Quest 2 controllers.
 
@@ -8,5 +8,5 @@ This is a workaround for [#149](https://github.com/Yellow-Dog-Man/Resonite-Issue
 
 ## Installation
 1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
-1. Place [InspectorScroll.dll](https://github.com/art0007i/InspectorScroll/releases/latest/download/InspectorScroll.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Place [InteractorScroll.dll](https://github.com/ShadowDragon128/InteractorScroll/releases/latest/download/InteractorScroll.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
 1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
